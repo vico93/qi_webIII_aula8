f41e3d9de1e2358cfa21133767f5bc2b5b9c649a
Vico <vpontin93@gmail.com>
2020-05-21 21:03:27 -0300

    Segundo exercício re-refeito
    Adicionado Gétchi e Sétchi

--- ProjExercicio1B/src/View/Main.java
+++ ProjExercicio1B/src/View/Main.java
@@ -14,19 +14,19 @@ public class Main {
 
         // Pede e lê cada dado - atributo
         System.out.print("Digite o nome do aluno: ");
-        a1.nome = entrada.nextLine();
+        a1.setNome(entrada.nextLine());
 
-        System.out.print("Digite a primeira nota de " + a1.nome + ": ");
-        a1.nota1 = entrada.nextDouble();
+        System.out.print("Digite a primeira nota de " + a1.getNome() + ": ");
+        a1.setNota1(entrada.nextDouble());
 
-        System.out.print("Digite a segunda nota de " + a1.nome + ": ");
-        a1.nota2 = entrada.nextDouble();
+        System.out.print("Digite a segunda nota de " + a1.getNome() + ": ");
+        a1.setNota2(entrada.nextDouble());
 
-        System.out.print("Digite a terceira nota de " + a1.nome + ": ");
-        a1.nota3 = entrada.nextDouble();
+        System.out.print("Digite a terceira nota de " + a1.getNome() + ": ");
+        a1.setNota3(entrada.nextDouble());
 
         // Invoca os métodos e mostra resultados
         System.out.println(a1);
-        System.out.println("Média do trimestre de " + a1.nome + ": " + String.format("%.2f", a1.calcularMedia()));
+        System.out.println("Média do trimestre de " + a1.getNome() + ": " + String.format("%.2f", a1.calcularMedia()));
     }
 }
